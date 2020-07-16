b03bd79c9a50af76dbecf99b0a4025d48aec4484
Henrique Holtz <henrique_holtz@hotmail.com>
2020-07-16 05:31:53 -0300

    Created personalized exception in board

--- ConsoleChass/Board/Board.cs
+++ ConsoleChass/Board/Board.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics;
+
 namespace board
 {
     class Board
@@ -14,14 +16,46 @@ namespace board
             Pieces = new Piece[lines, columns];
         }
 
+        public Piece Piece(Position position)
+        {
+            return Pieces[position.Line, position.Column];
+        }
+
         public Piece Piece(int line, int column)
         {
             return Pieces[line, column];
         }
+
         public void AddPiece(Piece piece, Position position)
         {
+            if (ExistPiece(position))
+            {
+                throw new BoardException("This position is already occupied.");
+            }
             Pieces[position.Line, position.Column] = piece;
             piece.Position = position;
+        }
+
+        public bool ExistPiece(Position position)
+        {
+            ValidatePosition(position); //BoardException
+            return Piece(position) != null;
+        }
+
+        public bool ValidPosition(Position position) 
+        {
+            if (position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidatePosition(Position position) 
+        {
+            if (!ValidPosition(position))
+            {
+                throw new BoardException("Invalid Position Guy!");
+            }
         }
     }
 }
--- ConsoleChass/Program.cs
+++ ConsoleChass/Program.cs
@@ -8,13 +8,22 @@ namespace ConsoleChass
     {
         static void Main(string[] args)
         {
-            Board Board = new Board(8, 8);
+            try
+            {
+                Board Board = new Board(8, 8);
 
-            Board.AddPiece(new Tower(Board, Color.Black),new Position(0, 0));
-            Board.AddPiece(new Tower(Board,Color.Black),new Position(1, 3));
-            Board.AddPiece(new King(Board,Color.Black),new Position(2, 4));
+                Board.AddPiece(new Tower(Board, Color.Black), new Position(0, 0));
+                Board.AddPiece(new Tower(Board, Color.Black), new Position(1, 3));
+                Board.AddPiece(new King(Board, Color.Black), new Position(2, 4));
 
-            Screen.PrintBoard(Board);
+                Screen.PrintBoard(Board);
+            }
+            catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
