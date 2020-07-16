363455a0aa6d4fcea1a86acf271dfbd5b35214f9
Henrique Holtz <henrique_holtz@hotmail.com>
2020-07-16 05:53:20 -0300

    Created chess position class

---
+++ ConsoleChess/chess/PositionChess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace chess
+{
+    class PositionChess
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public PositionChess(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
